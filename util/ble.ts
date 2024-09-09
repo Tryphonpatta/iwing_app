a5ee13a67c46cc221a16b296260631cb02288be8
parinya-ao <flim.parinya.ao@gmail.com>
2024-09-09 20:16:31 +0700

    "andorid connect BLE"

--- util/ble.ts
+++ util/ble.ts
@@ -1,11 +1,44 @@
 import { CHARACTERISTIC } from "@/enum/characteristic";
+import { PermissionsAndroid, Platform } from "react-native";
 import { BleManager } from "react-native-ble-plx";
+
+export const requestBluetoothPermission = async () => {
+  if (Platform.OS === 'ios') {
+    return true
+  }
+  if (Platform.OS === 'android' && PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION) {
+    const apiLevel = parseInt(Platform.Version.toString(), 10)
+
+    if (apiLevel < 31) {
+      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+      return granted === PermissionsAndroid.RESULTS.GRANTED
+    }
+    if (PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN && PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT) {
+      const result = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      ])
+
+      return (
+        result['android.permission.BLUETOOTH_CONNECT'] === PermissionsAndroid.RESULTS.GRANTED &&
+        result['android.permission.BLUETOOTH_SCAN'] === PermissionsAndroid.RESULTS.GRANTED &&
+        result['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED
+      )
+    }
+  }
+
+  console.log('Permission have not been granted');
+
+  return false
+}
 
 export const connectingDevice = async (
   bleManager: BleManager,
   deviceId: string,
   setConnectingDeviceList: (value: React.SetStateAction<string | null>) => void
 ) => {
+
   console.log("Connecting to", deviceId);
   setConnectingDeviceList(deviceId);
   await bleManager.connectToDevice(deviceId).then(async (device) => {
