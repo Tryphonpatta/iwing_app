47301ff84fbf70e693a0185626f677a10a3fc2f4
parinya-ao <flim.parinya.ao@gmail.com>
2024-09-23 16:46:47 +0700

    create encode.ts

---
+++ util/encode.ts
@@ -0,0 +1,20 @@
+function base64ToHex(base64: string): string {
+    const binaryString = atob(base64);
+    const byteArray = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        byteArray[i] = binaryString.charCodeAt(i);
+    }
+    let hexString = '';
+    let num;
+    for (let i = 0; i < byteArray.length; i++) {
+        const hex = byteArray[i].toString(16).padStart(2, '0');
+        hexString += hex;
+    }
+    return hexString;
+
+}
+
+
+const base64String = 'IA==';
+const hexResult = Number(base64ToHex(base64String));
+console.log(Number(hexResult));
