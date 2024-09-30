663fcb831624c10806be499e7b526f8fd3f540eb
SetthawutApichai <seegramza1@gmail.com>
2024-09-30 17:49:49 +0700

    testRunByRun

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,59 +1,18 @@
-import { Tabs } from "expo-router";
 import React from "react";
-import { Ionicons } from '@expo/vector-icons'; 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { ModuleContextProvider } from "./context/context";
 import { BleManagerProvider } from "./context/blecontext";
-
+import { Tabs } from 'expo-router'; 
+import { Ionicons } from '@expo/vector-icons'; // Import icons for better visual design
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <BleManagerProvider>
       <ModuleContextProvider>
-        <Tabs
-          screenOptions={{
-            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-            headerShown: false,
-          }}
-        >
-          <Tabs.Screen
-            name="index"
-            options={{
-              title: "Home",
-              tabBarIcon: ({ color, focused }) => (
-                <TabBarIcon
-                  name={focused ? "home" : "home-outline"}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="explore"
-            options={{
-              title: "Explore",
-              tabBarIcon: ({ color, focused }) => (
-                <TabBarIcon
-                  name={focused ? "code-slash" : "code-slash-outline"}
-                  color={color}
-                />
-              ),
-            }}
-          />
-        </Tabs>
-      </ModuleContextProvider>
-    </BleManagerProvider>
-  );
-}
-import { Tabs } from 'expo-router'; 
-import { Ionicons } from '@expo/vector-icons'; // Import icons for better visual design
-
-export default function Layout() {
-  return (
-    <Tabs
+      <Tabs
       screenOptions={{
         tabBarActiveTintColor: '#2f95dc',
         tabBarInactiveTintColor: 'gray',
@@ -98,5 +57,7 @@ export default function Layout() {
         }}
       />
     </Tabs>
+      </ModuleContextProvider>
+    </BleManagerProvider>
   );
 }
