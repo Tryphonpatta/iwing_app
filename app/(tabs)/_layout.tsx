ddeda263948afb6ce8ec984c4519f3b52dd6e9e0
parinya-ao <flim.parinya.ao@gmail.com>
2024-10-07 17:36:11 +0700

    "hello world"

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,50 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { ModuleContextProvider } from "./context/context";
+import { BleManagerProvider } from "./context/blecontext";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BleManagerProvider>
+      <ModuleContextProvider>
+        <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+            headerShown: false,
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Home",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "home" : "home-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="explore"
+            options={{
+              title: "Explore",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "code-slash" : "code-slash-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+        </Tabs>
+      </ModuleContextProvider>
+    </BleManagerProvider>
+  );
+}
--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { PropsWithChildren, useState } from "react";
 import { StyleSheet, TouchableOpacity, useColorScheme } from "react-native";
@@ -12,12 +13,26 @@ export function Collapsible({
 }: PropsWithChildren & { title: string }) {
   const [isOpen, setIsOpen] = useState(false);
   const theme = useColorScheme() ?? "light";
+=======
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { PropsWithChildren, useState } from 'react';
+import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Colors } from '@/constants/Colors';
+
+export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const theme = useColorScheme() ?? 'light';
+>>>>>>> origin/main
 
   return (
     <ThemedView>
       <TouchableOpacity
         style={styles.heading}
         onPress={() => setIsOpen((value) => !value)}
+<<<<<<< HEAD
         activeOpacity={0.8}
       >
         <Ionicons
@@ -30,14 +45,30 @@ export function Collapsible({
       {isOpen ? (
         <ThemedView style={styles.content}>{children}</ThemedView>
       ) : null}
+=======
+        activeOpacity={0.8}>
+        <Ionicons
+          name={isOpen ? 'chevron-down' : 'chevron-forward-outline'}
+          size={18}
+          color={theme === 'light' ? Colors.light.icon : Colors.dark.icon}
+        />
+        <ThemedText type="defaultSemiBold">{title}</ThemedText>
+      </TouchableOpacity>
+      {isOpen && <ThemedView style={styles.content}>{children}</ThemedView>}
+>>>>>>> origin/main
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   heading: {
+<<<<<<< HEAD
     flexDirection: "row",
     alignItems: "center",
+=======
+    flexDirection: 'row',
+    alignItems: 'center',
+>>>>>>> origin/main
     gap: 6,
   },
   content: {
